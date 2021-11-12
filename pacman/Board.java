062f4b65c6f9204be24668228d36411c1190c254
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-11-12 22:15:54 +0100

    Add files via upload

---
+++ pacman/Board.java
@@ -0,0 +1,138 @@
+package pacman;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/*
+Tamaño Bloque = cell
+Mapa puntos = 28 * 31
+Mapa Total = 30 * 41
+*/
+
+public class Board extends JPanel {
+
+    //Cell size (15 pixels) 
+    private int cell = 15;
+
+    //Images
+    private ImageIcon coco;
+    private ImageIcon cocoL1, cocoL2;
+    private ImageIcon cocoR1, cocoR2;
+    private ImageIcon cocoU1, cocoU2;
+    private ImageIcon cocoD1, cocoD2;
+
+    //Game board (Empty=0, Wall=1, Teleport=2, Special=3)
+    private int board[][] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+                     {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+                     {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
+                     {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
+                     {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
+                     {1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
+                     {0,0,0,0,0,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,0,0,0,0,0},
+                     {0,0,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,0,0,0},
+                     {0,0,0,0,0,1,0,1,1,0,1,1,1,3,3,1,1,1,0,1,1,0,1,0,0,0,0,0},
+                     {1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1},
+                     {2,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,2},
+                     {1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1},
+                     {0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0},
+                     {0,0,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,0,0,0,0},
+                     {0,0,0,0,0,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0},
+                     {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+                     {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
+                     {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1},
+                     {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
+                     {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
+                     {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
+                     {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
+                     {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
+                     {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};;
+
+    public Board() {
+        setSize(450,615);
+        setBackground(Color.BLACK);
+    }
+
+    private void loadImages() {
+
+        coco = new ImageIcon(getClass().getResource("../images/coco.gif")).getImage();
+        cocoL1 = new ImageIcon(getClass().getResource("../images/cocol1.gif")).getImage();
+        cocoL2 = new ImageIcon(getClass().getResource("../images/cocol2.gif")).getImage();
+        cocoR1 = new ImageIcon(getClass().getResource("../images/cocor1.gif")).getImage();
+        cocoR2 = new ImageIcon(getClass().getResource("../images/cocor2.gif")).getImage();
+        cocoU1 = new ImageIcon(getClass().getResource("../images/cocou1.gif")).getImage();
+        cocoU2 = new ImageIcon(getClass().getResource("../images/cocou2.gif")).getImage();
+        cocoD1 = new ImageIcon(getClass().getResource("../images/cocod1.gif")).getImage();
+        cocoD2 = new ImageIcon(getClass().getResource("../images/cocod2.gif")).getImage();
+
+
+    public void drawMaze(Graphics g) {
+        int aux = 0;
+        boolean first = true;
+        Graphics2D g2d = (Graphics2D) g;
+        Stroke stroke = new BasicStroke(3f);
+        g2d.setColor(Color.BLUE);
+        g2d.setStroke(stroke);
+        for (int i=0; i<31; i++) {
+            for (int j=0; j<28; j++) {
+                /*g2d.setColor(Color.WHITE);
+                g2d.drawLine(j*cell+8,i*cell+8,j*cell+8,i*cell+8);
+                g2d.setColor(Color.BLUE);*/
+                if (board[i][j]==1 && first) {
+                    aux = j;
+                    first = false;
+                }
+                if (board[i][j]==0 && !first) {
+                    g2d.drawLine(aux*cell+8,i*cell+8,(j-1)*cell+8,i*cell+8);
+                    first = true;
+                }
+                if (board[i][j]==1 && j == 27) {
+                    g2d.drawLine(aux*cell+8,i*cell+8,j*cell+8,i*cell+8);
+                    first = true;
+                }
+            }
+        }
+        for (int j=0; j<28; j++) {
+            for (int i=0; i<31; i++) {
+                if (board[i][j]==1 && first) {
+                    aux = i;
+                    first = false;
+                }
+                if (board[i][j]==0 && !first) {
+                    g2d.drawLine(j*cell+8,aux*cell+8,j*cell+8,(i-1)*cell+8);
+                    first = true;
+                }
+                if (board[i][j]==1 && i == 30) {
+                    g2d.drawLine(j*cell+8,aux*cell+8,j*cell+8,i*cell+8);
+                    first = true;
+                }
+            }
+        }
+        g2d.setColor(Color.PINK);
+        g2d.drawLine(13*cell+8,12*cell+8,14*cell+8,12*cell+8);
+    }
+
+    public void addFruit() {
+
+    }
+
+
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawMaze(g);
+    }
+
+}
