dd18c791a84f901e90ce9f1ecd04b1fa510902c0
Pau Villaverde Magrí <pauvima15@gmail.com>
2021-11-25 20:59:55 +0100

    Add files via upload

--- pacman/Board.java
+++ pacman/Board.java
@@ -16,12 +16,8 @@ import java.awt.event.KeyEvent;
 import java.awt.Stroke;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.Timer;
-
-import java.lang.Math;
 
 // MIRAR BOX LAYOUT PARA REDIMENSIONADO
 
@@ -44,23 +40,88 @@ public class Board extends JPanel implements ActionListener {
     private ImageIcon cocoU1, cocoU2;
     private ImageIcon cocoD1, cocoD2;
 
+    private Image redGhostImg;
+    private ImageIcon redGhostL;
+    private ImageIcon redGhostL1, redGhostL2;
+    private ImageIcon redGhostR1, redGhostR2;
+    private ImageIcon redGhostU1, redGhostU2;
+    private ImageIcon redGhostD1, redGhostD2;
+
+    private Image pinkGhostImg;
+    private ImageIcon pinkGhostL;
+    private ImageIcon pinkGhostR;
+    private ImageIcon pinkGhostU;
+    private ImageIcon pinkGhostD;
+
+    private Image blueGhostImg;
+    private ImageIcon blueGhostL;
+    private ImageIcon blueGhostR;
+    private ImageIcon blueGhostU;
+    private ImageIcon blueGhostD;
+
+    private Image orangeGhostImg;
+    private ImageIcon orangeGhostL;
+    private ImageIcon orangeGhostR;
+    private ImageIcon orangeGhostU;
+    private ImageIcon orangeGhostD;
+
+    private ImageIcon ghostW;
+    private ImageIcon ghostB;
+    private ImageIcon ghostWB;
+
+    private Image dotImg;
+    private ImageIcon dotYes, dotNo;
+
     // Coco
-    Coco coco;
+    Entity coco;
 
-    // Text
-    JLabel ready;
+    // Fantasmes
+    Entity redGhost;
+    Entity pinkGhost;
+    Entity blueGhost;
+    Entity orangeGhost;
 
-    // Timer per a animar i moure el coco
+    // Punts
+    Entity dot;
+
+    // Puntuació
+    int score;
+
+    // Timer per a moure el coco
     Timer timer = new Timer(20, this);
 
-    // Timer per al marcador
-    Timer score = new Timer(1000, new ActionListener() {
+    // Timer per a animar el coco
+    Timer mov = new Timer(90, new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-            
+            if (key == KeyEvent.VK_LEFT) {
+                if (cocoImg == cocoL1.getImage()) {
+                    setImage(cocoL2);
+                } else {
+                    setImage(cocoL1);
+                }
+            } else if (key == KeyEvent.VK_RIGHT) {
+                if (cocoImg == cocoR1.getImage()) {
+                    setImage(cocoR2);
+                } else {
+                    setImage(cocoR1);
+                }
+            } else if (key == KeyEvent.VK_UP) {
+                if (cocoImg == cocoU1.getImage()) {
+                    setImage(cocoU2);
+                } else {
+                    setImage(cocoU1);
+                }
+            } else if (key == KeyEvent.VK_DOWN) {
+                if (cocoImg == cocoD1.getImage()) {
+                    setImage(cocoD2);
+                } else {
+                    setImage(cocoD1);
+                }
+            }
         }
     });
 
-    // Game board (Empty=0, Wall=1, Teleport=2, Special=3)
+    // Game board (Dot=0, Wall=1, Teleport=2, Special=3, Empty=8)
     private int board[][] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                      {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
                      {1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1},
@@ -68,13 +129,13 @@ public class Board extends JPanel implements ActionListener {
                      {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                      {1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1},
                      {1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1},
-                     {1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1},
-                     {1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1},
-                     {1,1,1,1,0,1,0,3,3,3,3,3,0,1,0,1,1,1,1},
-                     {2,0,0,0,0,0,0,3,0,0,0,3,0,0,0,0,0,0,2},
-                     {1,1,1,1,0,1,0,3,3,3,3,3,0,1,0,1,1,1,1},
-                     {1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1},
-                     {1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1},
+                     {1,1,1,1,0,1,1,1,8,1,8,1,1,1,0,1,1,1,1},
+                     {1,1,1,1,0,1,8,8,8,8,8,8,8,1,0,1,1,1,1},
+                     {1,1,1,1,0,1,8,1,1,1,1,1,8,1,0,1,1,1,1},
+                     {2,8,8,8,0,8,8,1,0,0,0,1,8,8,0,8,8,8,2},
+                     {1,1,1,1,0,1,8,1,1,1,1,1,8,1,0,1,1,1,1},
+                     {1,1,1,1,0,1,8,8,8,8,8,8,8,1,0,1,1,1,1},
+                     {1,1,1,1,0,1,8,1,1,1,1,1,8,1,0,1,1,1,1},
                      {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
                      {1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1},
                      {1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1},
@@ -88,7 +149,6 @@ public class Board extends JPanel implements ActionListener {
         config();
         initVar();
         addKeyListener(new Key());
-        execute();
     }
 
     /**
@@ -98,12 +158,7 @@ public class Board extends JPanel implements ActionListener {
         setSize(608, 704);
         setBackground(Color.BLACK);
         setFocusable(true);
-        setLocationRelativeTo(this);
-        timer.setRepeats(true);
-        score.start();
-    }
-
-    private void setLocationRelativeTo(Board board2) {
+        mov.start();
     }
 
     /**
@@ -111,15 +166,28 @@ public class Board extends JPanel implements ActionListener {
      */
     public void initVar() {
         cell = 32;
-        coco = new Coco();
+        coco = new Entity("coco");
+        redGhost = new Entity("red");
+        pinkGhost = new Entity("pink");
+        blueGhost = new Entity("blue");
+        orangeGhost = new Entity("orange");
+
         loadImages();
         setImage(cocoP0);
+        redGhostImg = redGhostL.getImage();
+        pinkGhostImg = pinkGhostL.getImage();
+        blueGhostImg = blueGhostL.getImage();
+        orangeGhostImg = orangeGhostL.getImage();
+        dotImg = dotYes.getImage();
+
+        score = 0;
     }
 
     /**
      * Carrega les imatges
      */
     private void loadImages() {
+        // Coco
         cocoP0 = new ImageIcon(getClass().getResource("../images/coco.png"));
         cocoL1 = new ImageIcon(getClass().getResource("../images/cocol1.png"));
         cocoL2 = new ImageIcon(getClass().getResource("../images/cocol2.png"));
@@ -129,6 +197,28 @@ public class Board extends JPanel implements ActionListener {
         cocoU2 = new ImageIcon(getClass().getResource("../images/cocou2.png"));
         cocoD1 = new ImageIcon(getClass().getResource("../images/cocod1.png"));
         cocoD2 = new ImageIcon(getClass().getResource("../images/cocod2.png"));
+
+        // Red Ghost
+        redGhostL = new ImageIcon(getClass().getResource("../images/coco.png"));
+        redGhostL2 = new ImageIcon(getClass().getResource("../images/cocol2.png"));
+        redGhostR1 = new ImageIcon(getClass().getResource("../images/cocor1.png"));
+        redGhostR2 = new ImageIcon(getClass().getResource("../images/cocor2.png"));
+        redGhostU1 = new ImageIcon(getClass().getResource("../images/cocou1.png"));
+        redGhostU2 = new ImageIcon(getClass().getResource("../images/cocou2.png"));
+        redGhostD1 = new ImageIcon(getClass().getResource("../images/cocod1.png"));
+        redGhostD2 = new ImageIcon(getClass().getResource("../images/cocod2.png"));
+
+        // Pink Ghost
+        pinkGhostL = new ImageIcon(getClass().getResource("../images/coco.png"));
+
+        // Blue Ghost
+        blueGhostL = new ImageIcon(getClass().getResource("../images/coco.png"));
+
+        // Orange Ghost
+        orangeGhostL = new ImageIcon(getClass().getResource("../images/coco.png"));
+
+        // Punts
+        dotYes = new ImageIcon(getClass().getResource("../images/dot.png"));
     }
 
     /**
@@ -157,11 +247,16 @@ public class Board extends JPanel implements ActionListener {
                     g2d.setComposite(AlphaComposite.SrcOver.derive(0.1f)); /* Opacitat */
                     g2d.fillRect(i * (cell), j * (cell), 32, 32);
                     g2d.setComposite(AlphaComposite.SrcOver.derive(1.0f));
+                } else if (board[j][i] == 0) {
+                    dot = new Entity(i,j);
+                    g.drawImage(dotImg, dot.getX(), dot.getY(), this);
+                    score = score + 10;
+
                 }
             }
         }
 
-        // Dibuixem la casa dels Ghost(s)
+        /*// Dibuixem la casa dels Ghost(s)
         g2d.drawRect(7*cell,9*cell, 5*cell, 3*cell);
         g2d.setComposite(AlphaComposite.SrcOver.derive(0.1f));
         g2d.fillRect(7*cell,9*cell, 5*cell, 3*cell);
@@ -176,7 +271,7 @@ public class Board extends JPanel implements ActionListener {
         g2d.drawRect(9 * cell, 9 * cell, cell, cell / 2);
         g2d.setComposite(AlphaComposite.SrcOver.derive(0.1f));
         g2d.fillRect(9 * cell, 9 * cell, cell, cell / 2);
-        g2d.setComposite(AlphaComposite.SrcOver.derive(1.0f));
+        g2d.setComposite(AlphaComposite.SrcOver.derive(1.0f));*/
 
     }
 
@@ -187,10 +282,11 @@ public class Board extends JPanel implements ActionListener {
         switch (dir) {
             case 1:
                 //nextProbPos = ((coco.getX() - 12)/cell)%19;
-                if (board[(coco.getY()/cell)%22][((coco.getX() - 3)/cell)%19] != 0) {
+                if (board[(coco.getY()/cell)%22][((coco.getX() - 3)/cell)%19] != 0 && board[(coco.getY()/cell)%22][((coco.getX() - 3)/cell)%19] != 8) {
                     timer.stop();
                 } else {
                     coco.setX(coco.getX() - 3);
+                    board[(coco.getY()/cell)%22][((coco.getX() - 3)/cell)%19] = 8;
                 }
                 break;
             case 2:
@@ -217,12 +313,6 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
-    public void execute() {
-        ready = new JLabel("READY!");
-        ready.setLocation(SwingConstants.CENTER, 12*cell);
-        add(ready);
-    }
-
     public void addFruit() {
 
     }
@@ -246,46 +336,18 @@ public class Board extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (key == KeyEvent.VK_LEFT) {
             move(1);
-            if (anim == 0 || anim >= 5) {
-                setImage(cocoL1);
-                anim = 0;
-            } else if (anim == 3) {
-                setImage(cocoL2);
-            }
-            anim++;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
             move(2);
-            if (anim == 0 || anim >= 5) {
-                setImage(cocoR1);
-                anim = 0;
-            } else if (anim == 3) {
-                setImage(cocoR2);
-            }
-            anim++;
         }
 
         if (key == KeyEvent.VK_UP) {
             move(3);
-            if (anim == 0 || anim >= 5) {
-                setImage(cocoU1);
-                anim = 0;
-            } else if (anim == 3) {
-                setImage(cocoU2);
-            }
-            anim++;
         }
 
         if (key == KeyEvent.VK_DOWN) {
             move(4);
-            if (anim == 0 || anim >= 5) {
-                setImage(cocoD1);
-                anim = 0;
-            } else if (anim == 3) {
-                setImage(cocoD2);
-            }
-            anim++;
         }
     }
 
@@ -293,6 +355,10 @@ public class Board extends JPanel implements ActionListener {
         super.paint(g);
         drawMaze(g);
         g.drawImage(cocoImg, coco.getX(), coco.getY(), this);
+        g.drawImage(redGhostImg, redGhost.getX(), redGhost.getY(), this);
+        g.drawImage(pinkGhostImg, pinkGhost.getX(), pinkGhost.getY(), this);
+        g.drawImage(blueGhostImg, blueGhost.getX(), blueGhost.getY(), this);
+        g.drawImage(orangeGhostImg, orangeGhost.getX(), orangeGhost.getY(), this);
     }
 
 }
